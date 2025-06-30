bfa84144db4fe7ff37b89d7c5ff7d92b022e330f
Muhammad Al-Duais <132199789+Muhammad-alduais@users.noreply.github.com>
2025-06-30 22:22:33 +0300

    Update Primary Color to #4942E4

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -162,7 +162,7 @@ const Hero = () => {
                 href="#contact" 
                 className="flex items-center justify-center group w-full sm:w-auto text-center" 
                 style={{
-                  backgroundColor: '#FE5C02',
+                  backgroundColor: '#4942E4',
                   borderRadius: '1440px',
                   boxSizing: 'border-box',
                   color: '#FFFFFF',
